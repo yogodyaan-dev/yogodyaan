035194f99458d67aaa2985cc4fe7c29b6cf60b5f
gchakra2 <gourab.master@outlook.com>
2025-07-01 11:16:57 +0530

    Updated Header.tsx

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -49,6 +49,7 @@ export function Header() {
             ))}
           </nav>
 
+/* GC - Start - Remove Header Button         
           {/* CTA Button & User Menu */}
           <div className="hidden md:flex items-center space-x-4">
             <Link to="/book-class">
@@ -56,7 +57,7 @@ export function Header() {
                 Book Your Class
               </Button>
             </Link>
-            
+End - Start - Remove Header Button     */          
             {user ? (
               <div className="flex items-center space-x-2">
                 <div className="flex items-center space-x-2 text-gray-700">
